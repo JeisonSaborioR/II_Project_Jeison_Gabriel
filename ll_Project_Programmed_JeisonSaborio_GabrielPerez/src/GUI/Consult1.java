5fc824d0a75cdd5961a473d6a86ae4fee3be200d
GabrielPerezB <gabrielapb1996@gmail.com>
2015-06-14 23:22:28 -0600

    consulta 1 y 2

--- ll_Project_Programmed_JeisonSaborio_GabrielPerez/src/GUI/Consult1.java
+++ ll_Project_Programmed_JeisonSaborio_GabrielPerez/src/GUI/Consult1.java
@@ -5,7 +5,17 @@
  */
 package GUI;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import ll_project_programmed_jeisonsaborio_gabrielperez.GlobalVariables;
+import ll_project_programmed_jeisonsaborio_gabrielperez.Hotel;
+import ll_project_programmed_jeisonsaborio_gabrielperez.Reservation;
 
 /**
  *
@@ -30,22 +40,204 @@ public class Consult1 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        ComboBoxReservations = new javax.swing.JComboBox();
+        ButtonList = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        TableReservations = new javax.swing.JTable();
+        jButton5 = new javax.swing.JButton();
+        TextFieldEmailOfCustomer = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        ComboBoxReservations.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Pending", "Completed", "Cancelled" }));
+
+        ButtonList.setText("Search");
+        ButtonList.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ButtonListActionPerformed(evt);
+            }
+        });
+
+        TableReservations.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(TableReservations);
+
+        jButton5.setText("Back");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Email of customer");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 500, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton5)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(ComboBoxReservations, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(TextFieldEmailOfCustomer, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(ButtonList)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(ComboBoxReservations, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(ButtonList)
+                    .addComponent(TextFieldEmailOfCustomer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 245, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton5)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton5ActionPerformed
+
+    
+    ArrayList<Reservation> reservations = new ArrayList<>();
+    String customer;
+    private void ButtonListActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ButtonListActionPerformed
+        
+        String emailAddress = TextFieldEmailOfCustomer.getText().toString();
+        
+        for (int i = 0; i < GlobalVariables.getInstance().getAl().size(); i++) {
+            if (GlobalVariables.getInstance().getAl().get(i).getEmailAddress().equals(emailAddress)) {
+                customer = GlobalVariables.getInstance().getAl().get(i).getName();
+                reservations = GlobalVariables.getInstance().getAl().get(i).getListReservation();
+            }
+        }
+        
+
+        if(ComboBoxReservations.getSelectedIndex() == 0){
+            reservationsPending();
+        }else if(ComboBoxReservations.getSelectedIndex() == 1){
+            reservationsCompleted(); 
+        }else{
+            reservationsCancelled();
+        }
+    }//GEN-LAST:event_ButtonListActionPerformed
+    public void reservationsPending( ){
+        String []fila= new String[3];
+        String[]titulos = {"Customer","Entry Date","Departure Date"};
+        TableReservations.repaint();
+        DefaultTableModel model = new DefaultTableModel(null, titulos);
+        TableReservations.setModel(model);
+
+           
+            
+        for(int i = 0; i < reservations.size(); i++) {                
+            if(compare(reservations.get(i).getDepartureDate())==false){
+                fila[0]=customer;
+                fila[1]=reservations.get(i).getEntryDate();
+                fila[2]=reservations.get(i).getDepartureDate();
+                        
+                model.addRow(fila);
+                TableReservations.setModel(model);
+            }   
+        }
+    }
+    public void reservationsCancelled( ){
+        String []fila= new String[3];
+        String[]titulos = {"Customer","Entry Date","Departure Date"};
+        TableReservations.repaint();
+        DefaultTableModel model = new DefaultTableModel(null, titulos);
+        TableReservations.setModel(model);
+
+             
+            
+        for(int i = 0; i < reservations.size(); i++) {                
+            if(reservations.get(i).isPaid()== true){
+                fila[0]=customer;
+                fila[1]=reservations.get(i).getEntryDate();
+                fila[2]=reservations.get(i).getDepartureDate();
+                        
+                model.addRow(fila);
+                TableReservations.setModel(model);
+            }   
+        }        
+    }
+    
+    public void reservationsCompleted( ){
+        String []fila= new String[3];
+        String[]titulos = {"Customer","Entry Date","Departure Date"};
+        TableReservations.repaint();
+        DefaultTableModel model = new DefaultTableModel(null, titulos);
+        TableReservations.setModel(model);
+
+           
+            
+        for(int i = 0; i < reservations.size(); i++) {                
+            if(compare(reservations.get(i).getDepartureDate())==true){
+                fila[0]=customer;
+                fila[1]=reservations.get(i).getEntryDate();
+                fila[2]=reservations.get(i).getDepartureDate();
+                        
+                model.addRow(fila);
+                TableReservations.setModel(model);
+            }   
+        }
+    }
+    
+    
+    public static boolean compare(String date){
+        Date fechaActual = new Date();
+        SimpleDateFormat formateador = new SimpleDateFormat("dd/MM/yyyy");
+        String fechaSistema=formateador.format(fechaActual); 
+
+        try {
+
+            SimpleDateFormat formateador2 = new SimpleDateFormat("dd/MM/yyyy"); 
+            Date date1 = formateador2.parse(date);
+            Date systemDate = formateador2.parse(fechaSistema);
+
+            
+            if ( date1.before(systemDate) ){
+                return true;
+            }else if(systemDate.before(date1)){
+                return false;
+            }else{
+                return false;
+            }
+        } catch (ParseException e) {
+            System.out.println("Se Produjo un Error!!!  "+e.getMessage());
+        }  
+  
+        return true;
+    }
+    
+    
+    
+    
     /**
      * @param args the command line arguments
      */
@@ -82,5 +274,12 @@ public class Consult1 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton ButtonList;
+    private javax.swing.JComboBox ComboBoxReservations;
+    private javax.swing.JTable TableReservations;
+    private javax.swing.JTextField TextFieldEmailOfCustomer;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 }
